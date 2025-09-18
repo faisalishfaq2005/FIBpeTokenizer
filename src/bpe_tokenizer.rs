f968ffa94c80f662fdb4ad90028dc73d8aac1b23
faisalishfaq2005 <faisalishfaq59@gmail.com>
2025-09-18 10:12:23 +0500

    added pretokenization logic, special tokens logic in whole process, updated encode funtion

--- src/bpe_tokenizer.rs
+++ src/bpe_tokenizer.rs
@@ -5,8 +5,9 @@ use std::fs;
 use std::io;
 use std::time::Instant;
 use std::rc::Rc;
-use crate::tokenizer_tables::TokenTable;
+use crate::{pretokenizer::PreTokenization, tokenizer_tables::TokenTable};
 use rayon::prelude::*;
+use aho_corasick::AhoCorasick;
 
 #[derive(Debug, Clone)]
 pub struct Word {
@@ -14,11 +15,20 @@ pub struct Word {
     pub count: usize,
 }
 
+pub enum SpecialTokenRemovalMethod {
+    Simple,
+    AhoCorasick
+}
+
+
 //olso make a function for getting id given a special token
 //olso add the pre_tokenization techinque in the struct so you can give it to the train function which will get the raw data and then it will split data according to the given technique
 pub struct BpeTokenizer{
     input_text_path:String,
     target_vocab_size:usize,
+    pre_tokenizer:PreTokenization,
+    special_tokens:Vec<String>,
+    special_token_removal_method:SpecialTokenRemovalMethod,
     output_dir:String,
     token_table:TokenTable,
     merge_rules: Vec<(u32,u32)>,
@@ -26,19 +36,32 @@ pub struct BpeTokenizer{
     pairs_to_merge: HashMap<(u32,u32),u32>
     
 }
+#[derive(Clone,Debug)]
+pub enum TokenType {
+    WORD,
+    SUBWORD,
+    SPECIALTOKEN,
+
+}
+
+
 
 pub struct Encoder{
     pub original_text:String,
     pub tokens:Vec<String>,
-    pub ids:Vec<u32>
+    pub ids:Vec<u32>,
+    pub token_types: Vec<TokenType>
 }
 
 impl BpeTokenizer {
-    pub fn new(input_path:&str, vocab_size:usize, output_dir:&str) -> Self
+    pub fn new(input_path:&str, vocab_size:usize,pretokenizationtype:PreTokenization ,special_tokens:Vec<String>,special_token_removal_method:SpecialTokenRemovalMethod,output_dir:&str) -> Self
     {
         BpeTokenizer{
             input_text_path:input_path.to_string(),
             target_vocab_size: vocab_size,
+            pre_tokenizer:pretokenizationtype,
+            special_tokens:special_tokens,
+            special_token_removal_method:special_token_removal_method,
             output_dir:output_dir.to_string(),
             token_table:TokenTable::new(),
             merge_rules:Vec::new(),
@@ -56,6 +79,20 @@ impl Encoder{
         original_text:String::new(),
         tokens:Vec::new(),
         ids:Vec::new(),
+        token_types:Vec::new()
+        
+        }
+    }
+
+    pub fn get_token_type(&self,token:&str)-> TokenType{
+        if let Some(indx)=self.tokens.iter().position(|word| *word == token){
+            let token_type=self.token_types.get(indx).unwrap();
+            return token_type.clone();
+                 
+            
+        }
+        else{
+            panic!("the token does not exist");
         }
     }
 }
@@ -70,7 +107,29 @@ impl BpeTokenizer {
         match file_data {
             Ok(content) =>
                 {
-                        let tokens:Vec<String>= content.split_whitespace().map(|s| s.to_string()).collect();
+                        let mut cleaned_content_string:String=String::new();
+                        if !self.special_tokens.is_empty(){
+                            for special_token in self.special_tokens.iter(){
+                            self.token_table.get_or_insert_id(&special_token);
+                            }
+
+
+                            let remove_special_token_start=Instant::now();
+                            let cleaned_content=self.remove_special_tokens_from_corpus(&content);
+                            let remove_special_token_end=remove_special_token_start.elapsed();
+                            println!("removing special tokens took time: {:?}",remove_special_token_end);
+
+                            cleaned_content_string=cleaned_content;
+                        }
+                        else{
+                            cleaned_content_string=content;
+                        }
+
+                        let cleaned_content_str:&str=&cleaned_content_string;
+
+                        
+
+                        let tokens:Vec<String>= self.pre_tokenizer.pre_tokenize(cleaned_content_str);
         
                         let mut vocab:Vec<Word>=Vec::new();
 
@@ -130,34 +189,35 @@ impl BpeTokenizer {
                         let (ranks,pairs_to_merge)=build_ranks_and_pairs_to_merge(&self.merge_rules, &self.token_table);
                         self.ranks=ranks;
                         self.pairs_to_merge=pairs_to_merge;
+                        
 
                        
-                        println!("Checking if 'something</w>' exists during training...");
-                        // Assuming get_id returns u32 (not Option<u32>)
-                        let something_id = self.token_table.get_id("something</w>");
-                        println!("something</w> ID during training: {}", something_id);
+                        // println!("Checking if 'something</w>' exists during training...");
+                        // // Assuming get_id returns u32 (not Option<u32>)
+                        // let something_id = self.token_table.get_id("something</w>");
+                        // println!("something</w> ID during training: {}", something_id);
 
-                        println!("But was it added through BPE? Checking merge rules...");
-                        let mut found = false;
-                        for &(a, b) in &self.merge_rules {
-                            let token_a = self.token_table.get_token(a).expect("not found");
-                            let token_b = self.token_table.get_token(b).expect("not found");
-                            if format!("{}{}", token_a, token_b) == "something</w>" {
-                                println!("Found merge rule: '{}' + '{}'", token_a, token_b);
-                                found = true;
-                                break;
-                            }
-                        }
-                        if !found {
-                            println!("'something</w>' exists but no merge rule found! This is the bug.");
+                        // println!("But was it added through BPE? Checking merge rules...");
+                        // let mut found = false;
+                        // for &(a, b) in &self.merge_rules {
+                        //     let token_a = self.token_table.get_token(a).expect("not found");
+                        //     let token_b = self.token_table.get_token(b).expect("not found");
+                        //     if format!("{}{}", token_a, token_b) == "something</w>" {
+                        //         println!("Found merge rule: '{}' + '{}'", token_a, token_b);
+                        //         found = true;
+                        //         break;
+                        //     }
+                        // }
+                        // if !found {
+                        //     println!("'something</w>' exists but no merge rule found! This is the bug.");
                             
-                            // Let's also check what tokens 3279 and 221 actually are
-                            let token_3279 = self.token_table.get_token(3279).expect("not found");
-                            let token_221 = self.token_table.get_token(221).expect("not found");
-                            println!("Token 3279: '{}'", token_3279);
-                            println!("Token 221: '{}'", token_221);
-                            println!("Their combination: '{}{}'", token_3279, token_221);
-                        }
+                        //     // Let's also check what tokens 3279 and 221 actually are
+                        //     let token_3279 = self.token_table.get_token(3279).expect("not found");
+                        //     let token_221 = self.token_table.get_token(221).expect("not found");
+                        //     println!("Token 3279: '{}'", token_3279);
+                        //     println!("Token 221: '{}'", token_221);
+                        //     println!("Their combination: '{}{}'", token_3279, token_221);
+                        // }
                 }
             
             Err(er) =>{
@@ -171,14 +231,13 @@ impl BpeTokenizer {
         
     }
     pub fn encode(&self, raw_encoding_text: &str) -> Encoder {
-    let encoding_text_pretokenized: Vec<String> = raw_encoding_text.split_whitespace()
-        .map(|s| s.to_string())
-        .collect();
+    let encoding_text_pretokenized: Vec<String> = self.pre_tokenizer.pre_tokenize(raw_encoding_text);
     let end_of_word_id: u32 = self.token_table.get_id("</w>");
     let mut encoder_object = Encoder::new();
     encoder_object.original_text = raw_encoding_text.to_string();
     let mut words: Vec<Vec<u32>> = Vec::new();
 
+    
 
     for word in encoding_text_pretokenized.iter() {
         let mut char_ids: Vec<u32> = word.chars()
@@ -188,6 +247,7 @@ impl BpeTokenizer {
             })
             .collect();
         char_ids.push(end_of_word_id);
+        
         words.push(char_ids);
     }
 
@@ -227,6 +287,23 @@ impl BpeTokenizer {
         for id in word.iter() {
             encoder_object.ids.push(*id);
             if let Some(token) = self.token_table.get_token(*id) {
+                if token.to_string()=="</w>" && token.len()==4 {
+                    encoder_object.token_types.push(TokenType::SPECIALTOKEN);
+                }//or in other special tokens make another else if for it
+                else if token.to_string().ends_with("</w>") {
+                    let org_token=token.strip_suffix("</w>").unwrap();
+                    if encoding_text_pretokenized.contains(&org_token.to_string()){
+                        encoder_object.token_types.push(TokenType::WORD);
+                    }
+                    else{
+                        encoder_object.token_types.push(TokenType::SUBWORD);
+                    }
+
+                }
+                else{
+                    encoder_object.token_types.push(TokenType::SUBWORD);
+                }
+                     
                 encoder_object.tokens.push(token.to_string());
             }
         }
@@ -234,6 +311,261 @@ impl BpeTokenizer {
 
     encoder_object
 }
+
+
+
+
+ pub fn encode2(&self, raw_encoding_text: &str) -> Encoder {
+    let end_of_word_id: u32 = self.token_table.get_id("</w>");
+    let mut encoder_object = Encoder::new();
+    encoder_object.original_text = raw_encoding_text.to_string();
+
+    if !self.special_tokens.is_empty(){
+        let (segments,mut special_tokens_in_encoding_text)=self.extract_special_tokens(raw_encoding_text);
+        let mut total_text_pretokenized:Vec<String>=Vec::new();
+        let mut total_words:Vec<Vec<u32>>=Vec::new();
+        for segment in segments{
+            let encoding_segment_text_pretokenized:Vec<String>=self.pre_tokenizer.pre_tokenize(&segment);
+            total_text_pretokenized.extend(encoding_segment_text_pretokenized.clone());
+            let mut seg_words:Vec<Vec<u32>>=self.convert_words_to_u32vec(&encoding_segment_text_pretokenized, end_of_word_id);
+            self.encode_bpe_merging(&mut seg_words);
+            total_words.extend(seg_words);
+            if !special_tokens_in_encoding_text.is_empty(){
+                let special_token:String=special_tokens_in_encoding_text.remove(0);
+                let special_token_id=self.token_table.get_id(&special_token);
+                total_words.push(vec![special_token_id]);
+
+            }
+        }
+        self.prepare_encoder_object(&mut encoder_object, &total_words, &total_text_pretokenized);
+    }
+    else{
+        let encoding_text_pretokenized: Vec<String> = self.pre_tokenizer.pre_tokenize(raw_encoding_text);
+        let mut words: Vec<Vec<u32>> = self.convert_words_to_u32vec(&encoding_text_pretokenized, end_of_word_id);
+        self.encode_bpe_merging(&mut words);
+        self.prepare_encoder_object(&mut encoder_object, &words, &encoding_text_pretokenized);
+    }
+
+    
+    
+    
+    
+
+
+    encoder_object
+}
+
+pub fn convert_words_to_u32vec(&self, encoding_text_pretokenized:&Vec<String>,end_of_word_id:u32)-> Vec<Vec<u32>>{
+    let mut words:Vec<Vec<u32>>=Vec::new();
+    for word in encoding_text_pretokenized.iter() {
+        let mut char_ids: Vec<u32> = word.chars()
+            .map(|c| {
+                let ch = c.to_string();
+                self.token_table.get_id(&ch)
+            })
+            .collect();
+        char_ids.push(end_of_word_id);
+        
+        words.push(char_ids);
+
+    }
+    words
+}
+
+pub fn encode_bpe_merging(&self,words:&mut Vec<Vec<u32>>){
+    for word in words.iter_mut() {
+        let mut changed = true;
+        
+        while changed {
+            changed = false;
+            
+           
+            let lowest_rank_pair = word.windows(2)
+                .filter_map(|pair| {
+                    let key = (pair[0], pair[1]);
+                    self.ranks.get(&key).map(|&rank| (key, rank))
+                })
+                .min_by_key(|&(_, rank)| rank);
+
+            if let Some((pair, _)) = lowest_rank_pair {
+                if let Some(&merged_id) = self.pairs_to_merge.get(&pair) {
+                    
+                    let mut i = 0;
+                    while i < word.len().saturating_sub(1) {
+                        if word[i] == pair.0 && word[i + 1] == pair.1 {
+                            word.remove(i + 1);
+                            word[i] = merged_id;
+                            changed = true;  
+                           
+                        } else {
+                            i += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn prepare_encoder_object(&self,encoder_object:&mut Encoder,words:&Vec<Vec<u32>>,encoding_text_pretokenized:&Vec<String>){
+    for word in words.iter() {
+        for id in word.iter() {
+            encoder_object.ids.push(*id);
+            if let Some(token) = self.token_table.get_token(*id) {
+                if token.to_string()=="</w>" && token.len()==4 {
+                    encoder_object.token_types.push(TokenType::SPECIALTOKEN);
+                }//or in other special tokens make another else if for it
+                else if self.special_tokens.contains(&token.to_string()) {
+                    encoder_object.token_types.push(TokenType::SPECIALTOKEN);
+                }
+                else if token.to_string().ends_with("</w>") {
+                    let org_token=token.strip_suffix("</w>").unwrap();
+                    if encoding_text_pretokenized.contains(&org_token.to_string()){
+                        encoder_object.token_types.push(TokenType::WORD);
+                    }
+                    else{
+                        encoder_object.token_types.push(TokenType::SUBWORD);
+                    }
+
+                }
+                else{
+                    encoder_object.token_types.push(TokenType::SUBWORD);
+                }
+                     
+                encoder_object.tokens.push(token.to_string());
+            }
+        }
+    }
+}
+
+pub fn extract_special_tokens(&self,text: &str)-> (Vec<String>,Vec<String>){
+    let mut segments:Vec<String> =Vec::new();
+    let mut found_special_tokens:Vec<String>=Vec::new();
+    let mut current_pos=0;
+    let mut start_pos=0;
+
+    while current_pos<text.len() {
+        let mut speical_token_found=false;
+        for token in self.special_tokens.iter(){
+            if text[current_pos..].starts_with(token){
+                
+                segments.push(text[start_pos..current_pos].to_string());
+
+
+                found_special_tokens.push(token.clone());
+
+                start_pos= current_pos+token.len();
+                current_pos=start_pos;
+                speical_token_found=true;
+                break;
+            }
+            
+
+        }
+        if !speical_token_found{
+            current_pos+=1;
+        }
+    }
+    if start_pos<text.len(){
+        segments.push(text[start_pos..].to_string());
+    }
+    
+    (segments,found_special_tokens)
+}
+
+
+pub fn decode(&self,encoder_ids:&Vec<u32>)->String{
+    let mut decoded_text=String::new();
+    for id in encoder_ids.iter(){
+        if let Some(token)=self.token_table.get_token(*id){
+            if token.to_string().ends_with("</w>"){
+                
+                decoded_text=decoded_text+token+" ";
+            }
+            else if self.special_tokens.contains(&token.to_string()) {
+                decoded_text=decoded_text+token+" ";
+            }
+            else{
+                decoded_text=decoded_text+token;
+            }
+        }
+    }
+    decoded_text=decoded_text.replace("</w>", "");
+    return decoded_text;
+    
+
+}
+
+
+pub fn remove_special_tokens_from_corpus(&self,corpus:&str)-> String{
+    match self.special_token_removal_method {
+        SpecialTokenRemovalMethod::Simple => {
+            let mut clean_corpus=corpus.to_string();
+
+            for special_token in self.special_tokens.iter(){
+                clean_corpus=clean_corpus.replace(special_token, "");
+            }
+            clean_corpus
+        },
+        SpecialTokenRemovalMethod::AhoCorasick => {
+            let patterns: Vec<&str> = self.special_tokens
+            .iter()
+            .map(|s| s.as_str())
+            .collect();
+        
+            let ac = AhoCorasick::new(&patterns).unwrap();
+            let replacements:Vec<&str> = vec!["";patterns.len()];
+            ac.replace_all(corpus, &replacements).to_string()
+        }
+
+        
+    }
+    
+}
+
+
+// pub fn decode2(&self,encoder_ids:&Vec<u32>)->String{
+//     let mut decoded_text=String::new();
+//     for id in encoder_ids.iter(){
+//         if let Some(token)=self.token_table.get_token(*id){
+//             match self.pre_tokenizer {
+//                 PreTokenization::Punctuation => {
+//                     if token.to_string().ends_with("</w>"){
+//                         let punc_check= token.strip_suffix("</w>").unwrap();
+//                         if punc_check.len()==1{
+//                             let punch_check_ch:char=punc_check.chars().next().unwrap();
+//                             if punch_check_ch.is_ascii_punctuation(){
+                                
+//                             }
+//                         }
+//                         decoded_text=decoded_text+token+" ";
+//                     }
+//                     else{
+//                         decoded_text=decoded_text+token;
+//                     }
+//                 },
+//                 PreTokenization::Whitespace => {
+//                     if token.to_string().ends_with("</w>"){
+                
+//                 decoded_text=decoded_text+token+" ";
+//                     }
+//                     else{
+//                         decoded_text=decoded_text+token;
+//                     }
+//                 }
+
+//             }
+            
+//         }
+//     }
+//     decoded_text=decoded_text.replace("</w>", "");
+//     return decoded_text;
+    
+
+// }
+
+
+
 
 pub fn display(& self){
         
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use core::hash;
-use std::{collections::{HashMap, HashSet}, fmt::format, usize};
+use std::{any::type_name, collections::{HashMap, HashSet}, fmt::format, usize};
 use std::fs;
 use std::io;
 mod bpe_tokenizer;
 
-use bpe_tokenizer::{BpeTokenizer,Encoder};
+use bpe_tokenizer::{BpeTokenizer,Encoder,SpecialTokenRemovalMethod};
 use std::time::Instant;
+mod pretokenizer;
+use pretokenizer::PreTokenization;
+
+
 
 //use crate::bpe_tokenizer::Encoder;
 mod tokenizer_tables;
@@ -13,7 +17,9 @@ mod tokenizer_tables;
 
 
 fn main() {
-    let mut tokenizer=BpeTokenizer::new("corpus.txt", 4000, "out_dir");
+    let special_tokens:Vec<String>=vec!["<pad>".to_string(),"<mask>".to_string(),"<unk>".to_string(),"<eow>".to_string()];
+    let special_tokens2:Vec<String>=Vec::new();
+    let mut tokenizer=BpeTokenizer::new("corpus.txt", 4000,PreTokenization::Punctuation,special_tokens2,SpecialTokenRemovalMethod::AhoCorasick,"out_dir"); //set mechanism for default valies for special tokens wich is empty list and for special token remover fn
     let start=Instant::now();
 
     tokenizer.train();
@@ -21,20 +27,40 @@ fn main() {
     println!("training time completed in : {:?}",duration);
 
     //tokenizer.display();
-    let encoding_text:&str="return steal something business free Henry";
-    let encoded_text:Encoder=tokenizer.encode(&encoding_text);
-    tokenizer.get_id_by_token("return</w>".to_string());
-    tokenizer.get_id_by_token("something</w>".to_string());
-    tokenizer.get_merge_pair_from_pairs_to_merge_and_ranks((3279,221));
+    let encoding_text:&str="we are going i'm to the supermarket <pad>, on Friday, and <mask> Wednesday. on the plane there, will be some of magic but <eow>" ;
+    let encoded_text:Encoder=tokenizer.encode2(&encoding_text);
+   
     println!("{:?}",encoded_text.tokens);
     println!("{:?}",encoded_text.ids);
-    println!("Encoded ")
+    
+    println!("Encoded ");
+    let decoded=tokenizer.decode(&encoded_text.ids);
+    println!("decoded text: {}",decoded);
+    println!("{:?}",encoded_text.token_types);
+    let t_type=encoded_text.get_token_type("to</w>");
+    println!("{:?}",t_type);
+
+
+    
+    
 
 
 //give something like bool option wheather user wants the trained tokenized to be saved, meaning the file thing.
     
+    
+
+
 
     
 
 }
 
+
+//convert to library
+// handle file reading separately
+//handle the errors and communicate them effectively
+//handle match statements in decode fn for pre tokenization
+// include the punctuation type in tokentype and add token.isasciipunctuation to check for speciltokens currently it is for only </w>
+
+//make the match statements separate for each pretokenizer type in the decoder fn
+// do the comment written after tokenizer::new() in main
